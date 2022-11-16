853b3d8af46396f185090a6a973195da10df941c
Mike Muscato <Michael.Muscato@t-mobile.com>
2022-11-16 16:50:23 -0700

    Add Postgres TypeORM config for local development.  Update spacing for linting.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,30 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: 'tsconfig.json',
+      sourceType: 'module',
+    },
+    plugins: [
+      '@typescript-eslint/eslint-plugin',
+      '@darraghor/nestjs-typed'],
+    extends: [
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@darraghor/nestjs-typed/recommended',
+      '../code-standards/js/.eslintrc.js',
+    ],
+    root: true,
+    env: {
+      node: true,
+      jest: true,
+    },
+    ignorePatterns: ['.eslintrc.js'],
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'new-cap': 0, // Allows @Decorator() formatted decorator functions.
+      '@darraghor/nestjs-typed/api-method-should-specify-api-response':
+              'off',
+    },
 };
